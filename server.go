8aea9f1374c92691c098811c7554811069c11f32
ivan parra <ivantrips1@gmail.com>
2019-08-27 14:16:35 -0500

    Added more stuff

--- server.go
+++ server.go
@@ -4,9 +4,9 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "ok",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
